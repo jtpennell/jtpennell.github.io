2a00d59577d21e8512de40af7fb337ec0874ef3c
jtpennell <jayt24seven@gmail.com>
2018-08-13 20:44:53 +1200

    Updated my website
    Added a content to my portfolio and changed the overall aesthetic

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ function showTabA() {
    if (currentTab != "TabA") {
       currentTab = "TabA";
       showNoTabs();
+//      document.getElementById("TabA").style.backgroundColor = "#d9d9d9";
       document.getElementById("SectionA").style.display = "inline";
+      document.getElementById("a").style.outlineStyle = "solid";
+
    }
 }
 
@@ -17,8 +20,10 @@ function showTabB() {
    if (currentTab != "TabB") {
       currentTab = "TabB";
       showNoTabs();
-      document.getElementById("TabB").style.backgroundColor = "#d9d9d9";
+//      document.getElementById("TabB").style.backgroundColor = "#d9d9d9";
       document.getElementById("SectionB").style.display = "inline";
+      document.getElementById("b").style.outlineStyle = "solid";
+
    }
 }
 
@@ -26,24 +31,32 @@ function showTabC() {
    if (currentTab != "TabC") {
       currentTab = "TabC";
       showNoTabs();
-      document.getElementById("TabC").style.backgroundColor = "#d9d9d9";
+//      document.getElementById("TabC").style.backgroundColor = "#d9d9d9";
       document.getElementById("SectionC").style.display = "inline";
+         document.getElementById("c").style.outlineStyle = "solid";
+
    }
 }
+
 function showTabD() {
    if (currentTab != "TabD") {
       currentTab = "TabD";
       showNoTabs();
-      document.getElementById("TabD").style.backgroundColor = "#d9d9d9";
+      document.getElementById("d").style.outlineStyle = "solid";
       document.getElementById("SectionD").style.display = "inline";
    }
 }
 
 function showNoTabs() {
-   document.getElementById("TabA").style.backgroundColor = "#262626";
+   document.getElementById("TabA").style.backgroundColor = "transparent";
    document.getElementById("TabB").style.backgroundColor = "transparent";
    document.getElementById("TabC").style.backgroundColor = "transparent";
    document.getElementById("TabD").style.backgroundColor = "transparent";
+
+   document.getElementById("a").style.outlineStyle = "none";
+   document.getElementById("b").style.outlineStyle = "none";
+   document.getElementById("c").style.outlineStyle = "none";
+   document.getElementById("d").style.outlineStyle = "none";
 
    document.getElementById("SectionA").style.display = "none";
    document.getElementById("SectionB").style.display = "none";
